65138732dcd0d497ad168527fb6d44e1b3756eb1
Advik <advik.b@gmail.com>
2024-10-31 11:41:23 +0000

    Add run command to execute SQL queries and handle user registration checks

--- commands/register.py
+++ commands/register.py
@@ -1,8 +1,10 @@
 from .start import start
 from .credentials import credentials
+from .run import run
 from telegram.ext import Application, CommandHandler
 
 def register(app: Application):
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("credentials", credentials))
-
+    app.add_handler(CommandHandler("run", run))
+    
