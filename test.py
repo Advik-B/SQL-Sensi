eb26d021b54c5593fa83cf7e936ae454ff8a8948
Advik <advik.b@gmail.com>
2024-10-31 06:46:58 +0000

    Add database setup script to create and describe a test table

---
+++ test.py
@@ -0,0 +1,23 @@
+from backend.create_db import create_db, create_table
+from mysql.connector import connect
+
+
+from os import getenv as env
+conn = connect(
+    host=env("SQL_HOST"),
+    user=env("SQL_USER"),
+    password=env("SQL_PASSWORD")
+)
+
+create_db(conn, "test_db")
+conn.cursor().execute("USE test_db;")
+conn.commit()
+create_table(conn, "test_table", {"name": "VARCHAR(255)", "age": "INT"})
+
+
+# Run the desc command.
+cursor = conn.cursor()
+cursor.execute("DESC test_table")
+print(cursor.fetchall())
+cursor.close()
+conn.close()
