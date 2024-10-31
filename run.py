624f9ed1af13bf2edef0d05d8351630caebe238d
Advik <advik.b@gmail.com>
2024-10-31 12:54:32 +0000

    Load environment variables from .env file in main function

--- run.py
+++ run.py
@@ -3,8 +3,10 @@ from backend.user_management import init_db
 from backend.core import db
 from commands import register as register_commands
 from os import getenv as env
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv()
     token = env("TOKEN")
     init_db(db)
     app = Application.builder().token(token).build()
