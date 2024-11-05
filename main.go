d804df3f17a7ec025420598febbc7a7e40ff098e
Advik <advik.b@gmail.com>
2024-11-05 12:52:35 +0000

    Implement concurrent message printing in main function

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import io "fmt"
+import "fmt"
+import "time"
+
 
 func main() {
-	io.Println("Hello, World!")
+	c := make(chan string)
+	// Start a goroutine to send messages to the channel
+	go func() {
+		defer close(c)
+		for i := 0; i < 10; i++ {
+			c <- fmt.Sprintf("Hello, World! %d", i)
+			// Sleep for 1 second
+			time.Sleep(500 * time.Millisecond)
+		}
+		// Close the channel
+	}()
+	
+	fmt.Println("Hello, World!")
+	// Iterate over the channel and print the values until the channel is empty
+	for msg := range c {
+		fmt.Println(msg)
+	}
 }
