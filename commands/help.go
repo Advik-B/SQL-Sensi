eff57c406ead5b47e222783e5d9f7d7ea9d1eccf
Advik <advik.b@gmail.com>
2024-11-05 14:08:41 +0000

    Add help command to list available commands and usage details

---
+++ commands/help.go
@@ -0,0 +1,36 @@
+package commands
+
+import (
+	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"fmt"
+)
+
+
+func help(bot *telegram.BotAPI, message *telegram.Message) {
+	msg := telegram.NewMessage(message.Chat.ID, "")
+	if len(message.CommandArguments()) == 0 {
+		msg.Text = "Available commands:\n"
+		for _, command := range Commands {
+			msg.Text += command.Name + " - " + command.Description + "\n"
+		}
+	} else {
+		for _, command := range Commands {
+			if message.CommandArguments() == command.Name {
+				msg.Text = command.Name + "\n" + command.Description + "\n" + "Usage: " + command.Usage + "\n"
+				break
+			}
+		}
+		if msg.Text == "" {
+			msg.Text = fmt.Sprintf("Command %s not found", message.CommandArguments())
+		}
+	}
+}
+
+func HelpCommand() Command {
+	return Command{
+		Name: "help",
+		Description: "Show all available commands",
+		Handler: help,
+		Usage: "/help or /help <command>",
+	}
+}
