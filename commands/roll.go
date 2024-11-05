7440ba8b6b845daff78ef08a93c5d32206a2e60c
Advik <advik.b@gmail.com>
2024-11-05 14:28:23 +0000

    Add roll command to handle dice rolling in the commands package

---
+++ commands/roll.go
@@ -0,0 +1,19 @@
+package commands
+
+import (
+	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func RollCommand() Command {
+	return Command{
+		Name:        "roll",
+		Description: "Roll a dice",
+		Handler: func(bot *telegram.BotAPI, message *telegram.Message) {
+			msg := telegram.NewMessage(message.Chat.ID, "Rolling a dice...")
+			bot.Send(msg)
+			bot.Send(telegram.NewDice(message.Chat.ID))
+		},
+		Usage: "/roll",
+	}
+}
+
