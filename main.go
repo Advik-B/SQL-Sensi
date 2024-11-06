c7eee08e9bf64e94d901267b14c600f4fe073253
Advik <advik.b@gmail.com>
2024-11-06 15:13:15 +0000

    Remove commented-out database connection code from main.go

--- main.go
+++ main.go
@@ -1,48 +1,11 @@
 package main
 
 import (
-	"sql.sensi/database"
-	env "github.com/joho/godotenv"
+	// "sql.sensi/database"
+	// env "github.com/joho/godotenv"
+	"fmt"
 )
 
 func main() {
-	// Load the .env file
-	env.Load()
-	// Create a new MySQL instance from the environment variables
-	mysql, err := database.FromEnvironment()
-	if err != nil {
-		panic(err)
-	}
-
-	// Connect to the MySQL database
-	err = mysql.Connect()
-	if err != nil {
-		panic(err)
-	}
-	defer mysql.Disconnect()
-	
-	// Test connection to the MySQL database
-	err = mysql.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	mysql.CreateDatabase("test")
-	// mysql.UseDatabase("test")
-	whereami := mysql.WhereAmI() // Returns the current database name
-	println(whereami)
-
-	rows, err := mysql.ShowDatabases()
-	if err != nil {
-		panic(err)
-	}
-
-	for rows.Next() {
-		var name string
-		err = rows.Scan(&name)
-		if err != nil {
-			panic(err)
-		}
-		println(name)
-	}
+	fmt.Println(s)
 }
