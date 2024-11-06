7ab1673542993b172f2a3a24664c6ac1ed2a78fe
Advik <advik.b@gmail.com>
2024-11-06 16:53:46 +0000

    Enhance welcome messages with detailed commands and descriptions for new users

--- commands/start.go
+++ commands/start.go
@@ -8,8 +8,23 @@ import (
 	"sql.sensi/management"
 )
 
-var WelcomeBackMessage = "Hello %s!, welcome back to the SQL Sensi bot. You can now use the /help command to see the available commands."
-var WelcomeMessage = "Hello %s!, welcome to the SQL Sensi bot. Your account and database have been created. You can now use the /help command to see the available commands."
+var WelcomeBackMessage = `
+Hello %s!, welcome back to SQL Sensi, your account already exists!
+`
+var WelcomeMessage = `
+Hello %s!, welcome to SQL Sensi
+I will help you learn and develop with MySQL
+
+Here are some commands to get you started:
+- /sample - Get a few sample tables to play with
+- /sql - Run SQL queries
+- /ai - Get help from the AI to solve your MySQL problems
+- /start - Create your account/check the status of your account
+- /welcome - Show this message again
+... and many more
+
+You can use /help to see all the commands available
+`
 
 func start(bot *telegram.BotAPI, message *telegram.Message) {
 	if management.UserExists(&DB, message.From.ID) {
@@ -21,12 +36,22 @@ func start(bot *telegram.BotAPI, message *telegram.Message) {
 	log.Printf("User %s has been added to the database", message.From.FirstName)
 }
 
+func welcome(bot *telegram.BotAPI, message *telegram.Message) {
+	bot.Send(telegram.NewMessage(message.Chat.ID, fmt.Sprintf(WelcomeMessage, message.From.FirstName)))
+}
 
-func StartCommand() Command {
-	return Command{
+
+func init() {
+	Register(Command{
 		Name:        "start",
 		Description: "Start the bot and create your account",
 		Handler:     start,
 		Usage:       "/start",
-	}
+	})
+	Register(Command{
+		Name:        "welcome",
+		Description: "Show the welcome message again",
+		Handler:     welcome,
+		Usage:       "/welcome",
+	})
 }
