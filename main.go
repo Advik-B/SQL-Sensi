2f9ff48ce63061ef4959a887a68f92bb669730c5
Advik <advik.b@gmail.com>
2024-11-06 15:37:06 +0000

    Initialize database connection and prepare commands in main function

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	env "github.com/joho/godotenv"
 	"sql.sensi/commands"
+	"sql.sensi/database"
+	"sql.sensi/management"
 )
 
 func main() {
@@ -16,6 +18,12 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+	db, err := database.FromEnvironment()
+	if err != nil {
+		log.Panic(err)
+	}
+	management.PrepareDB(db)
+	commands.Initialize(db)
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
