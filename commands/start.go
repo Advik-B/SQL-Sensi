cce4c00a2afde2151cff45459179840f3133aae1
Advik <advik.b@gmail.com>
2024-11-06 15:37:18 +0000

    Add start command handling for user greetings in Telegram bot

---
+++ commands/start.go
@@ -0,0 +1,22 @@
+package commands
+
+import (
+	"fmt"
+	"log"
+
+	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"sql.sensi/management"
+)
+
+var WelcomeBackMessage = "Hello %s!, welcome back to the SQL Sensi bot. You can now use the /help command to see the available commands."
+var WelcomeMessage = "Hello %s!, welcome to the SQL Sensi bot. Your account and database have been created. You can now use the /help command to see the available commands."
+
+func start(bot *telegram.BotAPI, message *telegram.Message) {
+	if management.UserExists(&DB, message.From.ID) {
+		bot.Send(telegram.NewMessage(message.Chat.ID, fmt.Sprintf(WelcomeBackMessage, message.From.FirstName)))
+		return
+	}
+	management.UserFromTelegram(message.From, &DB)
+	bot.Send(telegram.NewMessage(message.Chat.ID, fmt.Sprintf(WelcomeMessage, message.From.FirstName)))
+	log.Printf("User %s has been added to the database", message.From.FirstName)
+}
