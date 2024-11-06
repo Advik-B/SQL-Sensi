a8020dacbcca42a72e2493550bc5aff7ca101417
Advik <advik.b@gmail.com>
2024-11-06 15:14:08 +0000

    Implement Telegram bot initialization and command handling in main.go

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-	// "sql.sensi/database"
-	// env "github.com/joho/godotenv"
-	"fmt"
+	"log"
+	"os"
+
+	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	env "github.com/joho/godotenv"
+	"sql.sensi/commands"
 )
 
 func main() {
-	fmt.Println(s)
+	env.Load() // Load .env file if it exists
+	token := os.Getenv("TELEGRAM_TOKEN")
+	bot, err := telegram.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+
+	u := telegram.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		switch {
+		case update.Message == nil:
+			continue
+		case update.Message.IsCommand():
+			go commands.Handle(bot, update.Message) // Pass the message to the command handler
+		}
+	}
 }
