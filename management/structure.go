10df22d601205ee673cccd9707cfcf0f46d6b9f9
Advik <advik.b@gmail.com>
2024-11-06 14:29:38 +0000

    Add User struct to manage user data for Telegram integration

---
+++ management/structure.go
@@ -0,0 +1,26 @@
+package management
+
+// Import timestamp package
+import (
+	"time"
+)
+
+// import telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+
+
+type User struct {
+	ID             int64
+	Username       string
+	FName          string
+	LName          string
+	LanguageCode   string
+	CreatedAt      time.Time
+	IsAdmin        bool
+	IsPremium      bool
+	SQLUsername    string
+	SQLPassword    string
+	SQLDBName      string
+	GeminiAPIKey   string
+}
+
