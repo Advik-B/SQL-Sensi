d4d202e417315293164c5a505bc91ed97b40666b
Advik <advik.b@gmail.com>
2024-11-07 15:42:35 +0000

    Set database context to 'telegram' in UpdateUser function

--- management/update.go
+++ management/update.go
@@ -10,6 +10,7 @@ func UpdateUser(user *User, db *database.MySQL) error {
 	if db.Conn == nil {
 		return fmt.Errorf("database connection is nil")
 	}
+	db.UseDatabase("telegram")
 	query := `
 		UPDATE users 
 		SET username = ?, first_name = ?, last_name = ?, language_code = ?, is_admin = ?, sql_username = ?, sql_password = ?, sql_db_name = ?, gemini_api_key = ? 
