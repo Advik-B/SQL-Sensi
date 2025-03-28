e01f39418b1a5813f7e97a57d4be31fe83cfb72b
Advik <advik.b@gmail.com>
2025-03-28 14:07:17 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"log"
-	"os"
-
+	"fmt"
 	"github.com/Advik-B/SQL-Sensi/commands"
 	"github.com/Advik-B/SQL-Sensi/database"
 	"github.com/Advik-B/SQL-Sensi/management"
 	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	env "github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
@@ -29,6 +30,9 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
+	// Start HTTP server in a separate goroutine
+	go startHTTPServer()
+
 	u := telegram.NewUpdate(0)
 	u.Timeout = 60
 
@@ -38,10 +42,21 @@ func main() {
 		switch {
 		case update.CallbackQuery != nil:
 			go commands.HandleCallback(bot, &update) // Pass the callback query to the callback handler
-		case update.Message.IsCommand():
+		case update.Message != nil && update.Message.IsCommand():
 			go commands.Handle(bot, update.Message) // Pass the message to the command handler
 		default:
 			log.Printf("Received a non-command message %v", update)
 		}
+	}
+}
+
+func startHTTPServer() {
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Bot is running and operational.")
+	})
+
+	log.Println("Starting HTTP server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Panicf("Failed to start HTTP server: %v", err)
 	}
 }
